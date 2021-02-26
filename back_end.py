856c44610a8e91afc88e9d9057fa7f49249b8986
Emre Cenk <emrecenk9@gmail.com>
2021-02-26 16:58:45 -0500

    a

--- back_end.py
+++ back_end.py
@@ -4,9 +4,20 @@
 
 
 from flask import Flask,render_template
+# from flask_sqlalchemy import SQLAlchemy
 
 
 my_app=Flask(__name__,template_folder="",static_folder="")
+# my_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+
+# db = SQLAlchemy(my_app) #initialize database
+
+
+# class user(db.Model):
+
+#     username = db
+
 
 @my_app.route('/',methods=["POST","GET"])
 def index():
@@ -15,6 +26,6 @@ def index():
 
 if __name__=="__main__":
     my_app.run(debug=True)
-
+    print("after running, it can wya")
 
     
