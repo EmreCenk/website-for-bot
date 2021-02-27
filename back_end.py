afa658b49f547313b9481f862eaad67edf232ea0
Emre Cenk <emrecenk9@gmail.com>
2021-02-27 18:00:31 -0500

    everything is working. yay!

--- back_end.py
+++ back_end.py
@@ -3,21 +3,27 @@
 
 
 from Instabot_2_4.Instagram_Bot_Class import instabot #The bot of the hour. He has finally arrived
-from flask import Flask,render_template,redirect,request
+from flask import Flask,render_template,redirect,request,url_for
 from time import perf_counter,sleep
 
 #INITIALIZE BOT:
-username="something"
-password="something"
+username="botusername"
+password="botpassword"
 
-bot = instabot(username,password,headless = False)
+bot = instabot(username,password,headless = True)
 bot.signin()
 my_app=Flask(__name__,template_folder="",static_folder="") #overriding the default directories flask searches for
 result="yes"
 
+@my_app.route('/verify',methods=["POST","GET"])
+def verify():
+    return render_template("verify.html")
+
+
 @my_app.route('/',methods=["POST","GET"])
 def index():
     if request.method=='POST':
+
         user=str(request.form['name'])
         # bot.follow(user)
         result = "I have sent a request to " + user
@@ -32,17 +38,14 @@ def index():
 
         if didwe:
             outcome=bot.evaluate_verification(message=didwe)
+            print("responding to verification")
             bot.respond_to_verification(user=user,outcome=outcome)
             if outcome == True:
                 bot.message_who_has_not_followed_back(user=user)
 
 
+        return redirect(url_for("verify"))
 
-
-
-
-
-        return ('', 204) #since we can't return nothing, we return this instead
     else:
 
         return render_template("index.html")
