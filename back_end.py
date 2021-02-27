d27f307b4253e0261d1946d8663b623ebfa1e930
Emre Cenk <emrecenk9@gmail.com>
2021-02-26 21:05:43 -0500

    Bot and Website fully linked

--- back_end.py
+++ back_end.py
@@ -5,28 +5,28 @@
 from Instabot_2_4.Instagram_Bot_Class import instabot #The bot of the hour. He has finally arrived
 from flask import Flask,render_template,redirect,request
 
-# #INITIALIZE BOT:
-# username="something"
-# password="password"
+#INITIALIZE BOT:
+username="something"
+password="password"
 
-# bot = instabot(username,password)
-# bot.signin()
+bot = instabot(username,password)
+bot.signin()
 my_app=Flask(__name__,template_folder="",static_folder="")
 
 
 @my_app.route('/',methods=["POST","GET"])
 def index():
     if request.method=='POST':
-        print("gven post")
-        return "alpha " + str(request.form['name'])
+        user=str(request.form['name'])
+        bot.follow(user)
+        return "I have sent a request to = " + user
     else:
-        print("gven get")
+
         return render_template("index.html")
  
 
 
 if __name__=="__main__":
-    my_app.run(debug=True)
+    my_app.run(debug=False)
     print("after running, it can wya")
-
     
